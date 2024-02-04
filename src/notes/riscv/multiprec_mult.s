f109f41a4006e5756c292d98591702a21d95d0f7
innocentzero <isfarulhaque@proton.me>
2024-02-04 15:38:12 +0530

    multiprecision arithmetic

--- src/notes/riscv/multiprec_mult.s
+++ src/notes/riscv/multiprec_mult.s
@@ -1,47 +1,60 @@
-### TODO: FIX THE LOGIC IT DOESN'T WORK
 .data
 # data goes here
-iter: .word 0x2 # number of chunks
-chunksize: .word 0x4 # bytes of the chunk, in this case word
-var1: .word 0x2
-	.word 0x0
-var2: .word 0x3
-	.word 0x0
-var3: .word 0x0
-	.word 0x0
+iter: .dword 0x2 # number of chunks
+chunksize: .dword 0x8 # bytes of the chunk, in this case word
+var1: .dword 0x2222222222222222
+	.dword 0x0000000000003333
+var2: .dword 0x10
+	.dword 0x3
+var3: .dword 0x0
+	.dword 0x0
+	.dword 0x0
+	.dword 0x0
 
 .section .text
 .global main
 
 main:
-lw a1, iter 
-lw a2, chunksize 
-mul s1, a1, a2
+ld a1, iter 
+ld a2, chunksize 
+mul s1, a1, a2 #adjusting max iterations to account for chunk size
 xor s2, s2, s2 #i = 0
-xor s3, s3, s3 #carry = 0
+xor s4, s4, s4 #carry = 0
 
-loop:
+outer_loop:
 bgeu s2, s1, return #i < k or exit
 
 la t1, var1 #t1 = &var1
 add t1, t1, s2 #t1 = &var1 + i
-lw t1, 0(t1) #t1 = var1[i]
+ld t1, 0(t1) #t1 = var1[i]
+
+xor s3, s3, s3 #j = 0
+
+inner_loop:
+bgeu s3, s1, inner_end # j < k or exit
 
 la t2, var2 #t2 = &var2
-add t2, t2, s2 #t2 = &var2 + i
-lw t2, 0(t2) #t2 = var2[i]
+add t2, t2, s3 #t2 = &var2 + j
+ld t2, 0(t2) #t2 = var2[j]
 
-mul t3, t1, t2 #lower bits of mutiplication
-add t3, t3, s3 #add carry over from prev multiplication
-mulh s3, t1, t2 #upper bits of multiplication for next iteration
+mul t3, t1, t2 #lower bits of var1[i] * var2[j]
+add t3, t3, s4 #add the previous carry here
+mulh s4, t1, t2 #upper bits of var1[i] * var2[j] set as the new carry
 
+la t4, var3 #t4 = &var3
+add t4, t4, s2 #t4 = &var3 + i
+add t4, t4, s3 #t4 = &var3 + i + j
 
-la t4, var3
-add t4, t4, s2
-sw t3, 0(t4) #store the final result
+ld t5, 0(t4) #fetch whatever the previous value in that block was
+add t5, t5, t3 #add the current multiplication to that value
+sd t5, 0(t4) #store the final result
 
-add s2, s2, a2
-j loop
+add s3, s3, a2 #j = j + 1
+j inner_loop
+
+inner_end:
+add s2, s2, a2 #i = i + 1
+j outer_loop
 
 return:
 ret
